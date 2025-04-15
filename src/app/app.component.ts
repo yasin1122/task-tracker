a2357e4a609e9659aedb418016a31111fda54e84
Yasin Cakal <whoisyasin@gmail.com>
2025-04-15 16:37:20 -0700

    starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent],
 })
-export class AppComponent {
-  title = 'task-tracker';
-}
+export class AppComponent {}
