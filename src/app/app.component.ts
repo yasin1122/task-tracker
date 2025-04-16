a188ba09c00699bcb0cc95ccef22ead92b5ff4a4
Yasin Cakal <whoisyasin@gmail.com>
2025-04-15 18:28:59 -0700

    pause point 34:46

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TasksComponent],
 })
 export class AppComponent {}
